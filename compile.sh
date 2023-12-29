4891e71a5f18980e6be3ab1775dd0948cc873c07
jakobrs <jakobrs100@gmail.com>
2023-12-29 18:16:44 +0100

    Lost track of what I changed

--- compile.sh
+++ compile.sh
@@ -6,7 +6,6 @@ args=(
   -C panic=abort                      # We really don't want to use the unwinding machinery
   -C link-arg=-nostartfiles           # Don't include libc stuff
   -C llvm-args=-x86-asm-syntax=intel  # obvious
-  --cfg include_panic_handler         # Workaround rust-analyzer being weird
   -C relocation-model=pic             # Generate position-independent code (static also
                                       # works if you know where the code will be loaded)
   -C target-feature=+crt-static       # unsure if this does anything here
--- main.rs
+++ main.rs
@@ -1,12 +1,11 @@
-#![feature(start)]
-#![feature(strict_provenance)]
 #![no_std]
 #![no_main]
 
-#[cfg(include_panic_handler)]
 use core::panic::PanicInfo;
 
 mod ctypes {
+    //! Type aliases for C types
+
     #![allow(non_camel_case_types)]
     pub type int = i32;
     pub type char = u8;
@@ -14,8 +13,26 @@ mod ctypes {
 }
 
 mod syscalls {
+    //! Direct syscalls
+
     use crate::ctypes::*;
     use core::arch::asm;
+
+    pub unsafe fn read(fd: int, data: *mut char, count: size_t) -> int {
+        let ret;
+        asm!(
+            "syscall",
+            in("rdi") fd,
+            in("rsi") data,
+            in("rdx") count,
+            in("rax") 0,
+            lateout("rax") ret,
+            lateout("rcx") _,
+            lateout("r11") _,
+            options(nostack)
+        );
+        ret
+    }
 
     pub unsafe fn write(fd: int, data: *const char, count: size_t) -> int {
         let ret;
@@ -44,17 +61,40 @@ mod syscalls {
 }
 
 mod utils {
+    //! Convenience functions
+
+    pub const STDIN: crate::ctypes::int = 0;
     pub const STDOUT: crate::ctypes::int = 1;
 
-    pub fn write(text: &str) {
+    /// Writes the text slice to stdout
+    pub fn write(s: &str) {
+        write_u8(s.as_bytes());
+    }
+
+    /// Same as `write` but for arbitrary data
+    pub fn write_u8(buf: &[u8]) {
         unsafe {
-            crate::syscalls::write(STDOUT, text.as_ptr() as *const _, text.len());
+            crate::syscalls::write(STDOUT, buf.as_ptr() as *const _, buf.len());
         }
+    }
+
+    #[inline(always)]
+    pub unsafe fn assume(cond: bool) {
+        if !cond {
+            core::hint::unreachable_unchecked();
+        }
+    }
+
+    #[inline(always)]
+    pub unsafe fn assume_in_range<T>(value: T, range: core::ops::Range<T>)
+    where
+        T: PartialOrd,
+    {
+        assume(range.contains(&value));
     }
 }
 
 #[panic_handler]
-#[cfg(include_panic_handler)]
 fn panic_handler(_panic_info: &PanicInfo) -> ! {
     loop {}
 }
@@ -62,9 +102,7 @@ fn panic_handler(_panic_info: &PanicInfo) -> ! {
 #[inline(never)]
 fn print_number(mut n: u32) {
     if n == 0 {
-        unsafe {
-            syscalls::write(utils::STDOUT, b"0" as *const _, 1);
-        }
+        utils::write("0");
         return;
     }
 
@@ -74,29 +112,37 @@ fn print_number(mut n: u32) {
     let mut length = 0;
 
     while n != 0 {
+        unsafe { utils::assume_in_range(SIZE - length - 1, 0..SIZE) }
         buf[SIZE - length - 1] = b'0' + (n % 10) as u8;
         n /= 10;
         length += 1;
     }
 
-    unsafe {
-        syscalls::write(
-            utils::STDOUT,
-            buf.as_ptr().map_addr(|addr| addr + SIZE - length as usize),
-            length,
-        );
-    }
+    unsafe { utils::assume_in_range(SIZE - length - 1, 0..SIZE) }
+    utils::write_u8(&buf[SIZE - length - 1..]);
 }
 
 #[no_mangle]
 fn _start() -> ! {
-    utils::write("Hello world!\n");
-    for i in 0..10 {
-        utils::write("Next number: ");
-        print_number(i);
-        utils::write("\n");
-    }
+    main();
+    unsafe { syscalls::exit(0) }
+}
+
+fn main() {
+    use core::str::FromStr;
+
     unsafe {
-        syscalls::exit(0);
+        utils::write("Number-tripling tool\n");
+        utils::write("Enter your number: ");
+
+        let mut buf = [0u8; 16];
+        let i = syscalls::read(5, &mut buf as *mut _, buf.len()) as usize;
+
+        utils::assume_in_range(i - 1, 0..buf.len());
+        let n: i32 =
+            i32::from_str(core::str::from_utf8_unchecked(&buf[..i - 1])).unwrap_unchecked();
+
+        utils::write("The result is: ");
+        print_number((n * 3) as u32);
     }
 }
